cd768d16937ce95751c0a0e65469408e439e7c2d
paragonsean <cos30degrees@gmail.com>
2023-03-16 03:05:03 -0400

    March 14, 2023 23:51:10

--- wp-content/themes/eduma-child/functions.php
+++ wp-content/themes/eduma-child/functions.php
@@ -5,3 +5,14 @@ function thim_child_enqueue_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'thim_child_enqueue_styles', 1000 );
+
+
+// checks if the user is logged in, if not, redirects to the user account page uses //pmpro_checkout_before_submit_button hook
+add_action('pmpro_checkout_before_submit_button', 'pmpro_checkout_redirect');
+
+function pmpro_checkout_redirect(){
+    if( !is_user_logged_in() ){
+        wp_redirect( home_url( '/account/' ) );
+        exit;
+    }
+}
